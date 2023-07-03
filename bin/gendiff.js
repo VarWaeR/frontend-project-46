de6d5bf4d06e36c044ebc30b6294d26394240bc5
Kirill <kirill.kltvkin@yandex.ru>
2023-07-03 12:17:24 +0300

    created bin and gendiff

---
+++ bin/gendiff.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+const program = require('commander');
+
+program
+  .name('gendiff')
+  .descriptions('Compares two configuration files and shows a difference.')
+  .version('0.0.1');
