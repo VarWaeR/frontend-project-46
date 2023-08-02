8512036418965d2e238892c2daad7449db6f1cb7
Kirill <kirill.kltvkin@yandex.ru>
2023-08-02 13:30:27 +0300

    step 3 complete

--- src/index.js
+++ src/index.js
@@ -13,19 +13,13 @@ const genDiff = (filepath1, filepath2) => {
   const data2 = getFileText(filepath2);
   const parcedData1 = JSON.parse(data1);
   const parcedData2 = JSON.parse(data2);
-  console.log(parcedData1);
-  console.log(data2);
   const keys = _.sortBy(_.union(_.keys(parcedData1), _.keys(parcedData2)));
-  console.log(keys);
   const diff = keys.map((key) => {
     if (!Object.hasOwn(parcedData1, key)) {
       return { key, type: 'added', value: parcedData2[key] };
     }
     if (!Object.hasOwn(parcedData2, key)) {
       return { key, type: 'deleted', value: parcedData1[key] };
-    }
-    if (_.isObject(parcedData1[key]) && _.isObject(parcedData2[key])) {
-      return { key, type: 'nested', children: buildDiff(parcedData1[key], parcedData2[key]) };
     }
     if (parcedData1[key] !== parcedData2[key]) {
       return {
