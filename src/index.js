4a4c9a4744bf7dbd6bf186ca72655c8f4b2eb83f
Kirill <kirill.kltvkin@yandex.ru>
2023-08-02 12:38:01 +0300

    fixing encode

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import _ from 'lodash';
 import { readFileSync } from 'fs';
 import path from 'path';
 
-console.log('started')
-
 const getFileText = (filename) => {
   const filePath = path.resolve(process.cwd(), filename);
   const data = readFileSync(filePath, 'utf-8');
@@ -11,24 +9,22 @@ const getFileText = (filename) => {
 };
 
 const genDiff = (filepath1, filepath2) => {
-  console.log('started');
   const data1 = getFileText(filepath1);
   const data2 = getFileText(filepath2);
-  console.log('data step complit');
+  console.log(data1);
   const keysData1 = Object.keys(data1);
   const keysData2 = Object.keys(data2);
-  console.log('keys step complit');
+  console.log(keysData1);
   const result = {};
-  console.log('result step complit');
   const keys = _.sortBy(_.uniq([...keysData1, ...keysData2]));
   keys.map((key) => {
-    if (!Object.hasOwn(data1, key)) {
+    if (!Object.hasOwn(keysData1, key)) {
       return result[key] = 'added';
     }
-    if (!Object.hasOwn(data2, key)) {
+    if (!Object.hasOwn(keysData2, key)) {
       return result[key] = 'deleted';
     }
-    if (data1[key] !== data2[key]) {
+    if (keysData1[key] !== keysData2[key]) {
       return result[key] = 'changed';
     }
     console.log(key)
