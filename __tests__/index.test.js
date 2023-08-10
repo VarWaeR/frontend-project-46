96b633ddad0fc419f2f0177a57bfbfab744e5ac7
VarWaeR <kirill.kltvkin@yandex.ru>
2023-08-10 14:08:47 +0300

    added test for flat files

---
+++ __tests__/index.test.js
@@ -0,0 +1,17 @@
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+import { readFileSync } from 'fs';
+import genDiff from '../src/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+
+const expected = readFileSync(path.resolve(process.cwd(), '__fixtures__/expected-json.txt'), 'utf-8');
+
+test('test flat files', () => {
+    const file1 = getFixturePath('file1.json');
+    const file2 = getFixturePath('file2.json');
+    expect(genDiff(file1, file2)).toEqual(expected);
+});
