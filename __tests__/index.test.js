1d23a54571c5979ef6afcd48d6ec762b56c95ec3
VarWaeR <kirill.kltvkin@yandex.ru>
2023-08-11 16:08:20 +0300

    added yaml tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -10,8 +10,10 @@ const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', 
 
 const expected = readFileSync(path.resolve(process.cwd(), '__fixtures__/expected-json.txt'), 'utf-8');
 
-test('flat files', () => {
-  const file1 = getFixturePath('file1.json');
-  const file2 = getFixturePath('file2.json');
+const extensions = ['json', 'yml'];
+
+test.each(extensions)('flat files', (extension) => {
+  const file1 = getFixturePath(`file1.${extension}`);
+  const file2 = getFixturePath(`file2.${extension}`);
   expect(genDiff(file1, file2)).toEqual(expected);
 });
