a30713635c3a1c4b4d458ad3ed9db74f8bd90ffd
VarWaeR <kirill.kltvkin@yandex.ru>
2023-08-16 21:31:08 +0300

    added new module getBuiltDiff

---
+++ src/__utils__/getBuiltDiff.js
@@ -0,0 +1,26 @@
+import _ from 'lodash';
+
+const getBuiltDiff = (parcedData1, parcedData2) => {
+    const keys = _.sortBy(_.union(_.keys(parcedData1), _.keys(parcedData2)));
+    const diff = keys.map((key) => {
+      if (!Object.hasOwn(parcedData1, key)) {
+        return { key, type: 'added', value: parcedData2[key] };
+      }
+      if (!Object.hasOwn(parcedData2, key)) {
+        return { key, type: 'deleted', value: parcedData1[key] };
+      }
+      if (parcedData1[key] !== parcedData2[key]) {
+        return {
+          key, type: 'changed', oldValue: parcedData1[key], newValue: parcedData2[key],
+        };
+      }
+      if (_.isObject(parcedData1[key]) && _.isObject(parcedData2[key])) {
+        return { key, type: 'nested', children: getBuiltDiff(parcedData1[key], parcedData2[key]) };
+      }
+      return { key, type: 'unchanged', value: parcedData1[key] };
+    });
+  
+    return diff;
+  };
+
+  export default getBuiltDiff;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import { readFileSync } from 'fs';
 import path from 'path';
-import _ from 'lodash';
-import stylish from './__utils__/stylish.js';
-import getParceData from './__utils__/parsers.js';
+import getParcedData from './__utils__/parsers.js';
+import buildDiff from './__utils__/buildDiff.js';
+import getFormatData from './__utils__/formatters/index.js';
 
 const getFileText = (filename) => {
   const filePath = path.resolve(process.cwd(), filename);
@@ -17,27 +17,12 @@ const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
   const fileExt1 = path.extname(filepath1);
   const fileExt2 = path.extname(filepath2);
 
-  const parcedData1 = getParceData(data1, fileExt1);
-  const parcedData2 = getParceData(data2, fileExt2);
+  const parcedData1 = getParcedData(data1, fileExt1);  
+  const parcedData2 = getParcedData(data2, fileExt2);
 
-  const keys = _.sortBy(_.union(_.keys(parcedData1), _.keys(parcedData2)));
+  const diff = buildDiff(parcedData1, parcedData2);
 
-  const diff = keys.map((key) => {
-    if (!Object.hasOwn(parcedData1, key)) {
-      return { key, type: 'added', value: parcedData2[key] };
-    }
-    if (!Object.hasOwn(parcedData2, key)) {
-      return { key, type: 'deleted', value: parcedData1[key] };
-    }
-    if (parcedData1[key] !== parcedData2[key]) {
-      return {
-        key, type: 'changed', oldValue: parcedData1[key], newValue: parcedData2[key],
-      };
-    }
-    return { key, type: 'unchanged', value: parcedData1[key] };
-  });
-
-  return stylish(diff);
+  return getFormatData(diff, formatName);
 };
 
 export default genDiff;
