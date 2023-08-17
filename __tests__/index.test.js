d849b31959958720c372ea3fa0434f1d53cb1707
VarWaeR <kirill.kltvkin@yandex.ru>
2023-08-17 13:09:22 +0300

    added tests for plain formatter

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -8,13 +8,15 @@ const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-const expected = readFileSync(path.resolve(process.cwd(), '__fixtures__/expected-json.txt'), 'utf-8');
+const stylish = readFileSync(path.resolve(process.cwd(), '__fixtures__/expected-json.txt'), 'utf-8');
+const plain = readFileSync(path.resolve(process.cwd(), '__fixtures__/expected-plain.txt'), 'utf-8');
 
 const extensions = ['json', 'yml'];
 
 test.each(extensions)('%s format', (extension) => {
   const file1 = getFixturePath(`file1.${extension}`);
   const file2 = getFixturePath(`file2.${extension}`);
-  expect(genDiff(file1, file2)).toEqual(expected);
-  expect(genDiff(file1, file2, 'stylish')).toEqual(expected);
+  expect(genDiff(file1, file2)).toEqual(stylish);
+  expect(genDiff(file1, file2, 'stylish')).toEqual(stylish);
+  expect(genDiff(file1, file2, 'plain')).toEqual(plain);
 });
