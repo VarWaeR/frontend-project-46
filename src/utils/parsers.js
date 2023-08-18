c3e140adc0175ac0c61c9e87b69351b128ab652a
VarWaeR <kirill.kltvkin@yandex.ru>
2023-08-18 15:30:00 +0300

    added getExtension to parsers.js

--- src/utils/parsers.js
+++ src/utils/parsers.js
@@ -1,7 +1,9 @@
 import yaml from 'js-yaml';
 
+const getExtension = (ext) => ext.slice(1);
+
 const getParcedData = (data, ext) => {
-  const extension = ext.slice(1);
+  const extension = getExtension(ext)
   switch (extension) {
     case 'json':
       return JSON.parse(data);
