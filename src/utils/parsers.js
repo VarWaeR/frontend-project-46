070aa837283f32afb45233dc80c0b45f7de53fc9
Kirill <kirill.kltvkin@yandex.ru>
2023-08-18 15:00:19 +0300

    fixed lint mistake

--- src/utils/parsers.js
+++ src/utils/parsers.js
@@ -3,7 +3,7 @@ import yaml from 'js-yaml';
 const getExtension = (ext) => ext.slice(1);
 
 const getParcedData = (data, ext) => {
-  const extension = getExtension(ext)
+  const extension = getExtension(ext);
   switch (extension) {
     case 'json':
       return JSON.parse(data);
